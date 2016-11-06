6db49e0a694406d5032168af11de48a84d018d5a
Michael Peterman <michaelpeterman@wightman.ca>
2016-11-06 13:23:49 -0500

    Initial commit - Added a most of the room descriptions and button label words n stuff

--- LostV2/Form1.Designer.cs
+++ LostV2/Form1.Designer.cs
@@ -30,82 +30,157 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.outputLabel = new System.Windows.Forms.Label();
-            this.redLabel = new System.Windows.Forms.Label();
             this.blueLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
             this.blueImage = new System.Windows.Forms.PictureBox();
             this.redImage = new System.Windows.Forms.PictureBox();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.yellowLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.roomLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.blueImage)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.redImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // outputLabel
             // 
             this.outputLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.outputLabel.ForeColor = System.Drawing.Color.White;
-            this.outputLabel.Location = new System.Drawing.Point(36, 43);
+            this.outputLabel.Location = new System.Drawing.Point(24, 50);
+            this.outputLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.outputLabel.Name = "outputLabel";
-            this.outputLabel.Size = new System.Drawing.Size(509, 396);
+            this.outputLabel.Size = new System.Drawing.Size(339, 235);
             this.outputLabel.TabIndex = 2;
             this.outputLabel.Text = "label1";
-            // 
-            // redLabel
-            // 
-            this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.redLabel.ForeColor = System.Drawing.Color.White;
-            this.redLabel.Location = new System.Drawing.Point(101, 481);
-            this.redLabel.Name = "redLabel";
-            this.redLabel.Size = new System.Drawing.Size(386, 35);
-            this.redLabel.TabIndex = 3;
-            this.redLabel.Text = "label1";
             // 
             // blueLabel
             // 
             this.blueLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.blueLabel.ForeColor = System.Drawing.Color.White;
-            this.blueLabel.Location = new System.Drawing.Point(101, 537);
+            this.blueLabel.Location = new System.Drawing.Point(67, 313);
+            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.blueLabel.Name = "blueLabel";
-            this.blueLabel.Size = new System.Drawing.Size(386, 35);
-            this.blueLabel.TabIndex = 4;
+            this.blueLabel.Size = new System.Drawing.Size(257, 23);
+            this.blueLabel.TabIndex = 3;
             this.blueLabel.Text = "label1";
+            // 
+            // redLabel
+            // 
+            this.redLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(67, 349);
+            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(257, 23);
+            this.redLabel.TabIndex = 4;
+            this.redLabel.Text = "label1";
             // 
             // blueImage
             // 
-            this.blueImage.Image = global::LostV2.Properties.Resources.blue_50x50;
-            this.blueImage.Location = new System.Drawing.Point(36, 522);
+            this.blueImage.Image = global::LostV2.Properties.Resources.red_50x50;
+            this.blueImage.Location = new System.Drawing.Point(24, 339);
+            this.blueImage.Margin = new System.Windows.Forms.Padding(2);
             this.blueImage.Name = "blueImage";
-            this.blueImage.Size = new System.Drawing.Size(59, 50);
+            this.blueImage.Size = new System.Drawing.Size(39, 32);
             this.blueImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.blueImage.TabIndex = 1;
             this.blueImage.TabStop = false;
             // 
             // redImage
             // 
-            this.redImage.Image = global::LostV2.Properties.Resources.red_50x50;
-            this.redImage.Location = new System.Drawing.Point(36, 466);
+            this.redImage.Image = global::LostV2.Properties.Resources.blue_50x50;
+            this.redImage.Location = new System.Drawing.Point(24, 303);
+            this.redImage.Margin = new System.Windows.Forms.Padding(2);
             this.redImage.Name = "redImage";
-            this.redImage.Size = new System.Drawing.Size(59, 50);
+            this.redImage.Size = new System.Drawing.Size(39, 32);
             this.redImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.redImage.TabIndex = 0;
             this.redImage.TabStop = false;
             // 
+            // greenLabel
+            // 
+            this.greenLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenLabel.ForeColor = System.Drawing.Color.White;
+            this.greenLabel.Location = new System.Drawing.Point(67, 421);
+            this.greenLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(257, 23);
+            this.greenLabel.TabIndex = 8;
+            this.greenLabel.Text = "label1";
+            // 
+            // yellowLabel
+            // 
+            this.yellowLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.yellowLabel.ForeColor = System.Drawing.Color.White;
+            this.yellowLabel.Location = new System.Drawing.Point(67, 385);
+            this.yellowLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.yellowLabel.Name = "yellowLabel";
+            this.yellowLabel.Size = new System.Drawing.Size(257, 23);
+            this.yellowLabel.TabIndex = 7;
+            this.yellowLabel.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LostV2.Properties.Resources.green_50x50;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 411);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 32);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::LostV2.Properties.Resources.yellow_50x50;
+            this.pictureBox2.Location = new System.Drawing.Point(24, 375);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(39, 32);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // roomLabel
+            // 
+            this.roomLabel.Font = new System.Drawing.Font("Courier New", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roomLabel.ForeColor = System.Drawing.Color.White;
+            this.roomLabel.Location = new System.Drawing.Point(11, 9);
+            this.roomLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.roomLabel.Name = "roomLabel";
+            this.roomLabel.Size = new System.Drawing.Size(257, 23);
+            this.roomLabel.TabIndex = 9;
+            this.roomLabel.Text = "label1";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(579, 649);
-            this.Controls.Add(this.blueLabel);
+            this.ClientSize = new System.Drawing.Size(734, 462);
+            this.Controls.Add(this.roomLabel);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.yellowLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.blueLabel);
             this.Controls.Add(this.outputLabel);
             this.Controls.Add(this.blueImage);
             this.Controls.Add(this.redImage);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Lost in a Forest";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.blueImage)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.redImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -115,8 +190,13 @@
         private System.Windows.Forms.PictureBox redImage;
         private System.Windows.Forms.PictureBox blueImage;
         private System.Windows.Forms.Label outputLabel;
-        private System.Windows.Forms.Label redLabel;
         private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label yellowLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label roomLabel;
     }
 }
 
--- LostV2/Form1.cs
+++ LostV2/Form1.cs
@@ -29,31 +29,268 @@ namespace LostV2
         {
             /// check to see what button has been pressed and advance
             /// to the next appropriate scene
-            if (e.KeyCode == Keys.M)       //red button press
+            if (e.KeyCode == Keys.M)           //red button press
             {
+                switch (scene)
+                {
+                    case 0:
+                        outputLabel.Text = "Don't be a bitch";
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    default:
+                        break;
+                }
 
             }
-            else if (e.KeyCode == Keys.B)  //blue button press
+            else if (e.KeyCode == Keys.B)      //blue button press
             {
-
+                switch (scene)
+                {
+                    case 0:
+                        scene = 1;
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (e.KeyCode == Keys.N)      //yellow button press
+            {
+                switch (scene)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (e.KeyCode == Keys.Space)  //green button press
+            {
+                switch (scene)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    default:
+                        break;
+                }
             }
 
             /// Display text and game options to screen based on the current scene
             switch (scene)
             {
                 case 0:  //start scene  
+                    outputLabel.Text = "You are an adventurer passing through the town of Phandalin. While at the general store, the owner mentioned some trouble the town had been having with a local gang called the Redbrands, who are led by a wizard named Glasstaff. He asks if you could help out. You agree, and have managed to track them to Tresandar Manor. After investigating the ruins, you find a cellar.";
+                    roomLabel.Text = "Tresandar Ruins";
+                    blueLabel.Text = "Enter the cellar";
+                    redLabel.Text = "Don't enter the cellar";
+                    yellowLabel.Text = "";
+                    greenLabel.Text = "";
                     break;
-                case 1:   
+                case 1:
+                    outputLabel.Text = "The door opens onto a five-foot-wide landing fifteen feet above a large cellar, with stone steps descending to the floor in two short flights. There is a door on the western wall, while another door stands beneath the stairs to the north. A large stone cistern occupies the western part of the room, whose walls are lined with kegs and barrels.";
+                    roomLabel.Text = "Cellar";
+                    blueLabel.Text = "Go west";
+                    redLabel.Text = "Search the barrels";
+                    yellowLabel.Text = "Go north";
+                    greenLabel.Text = "Search the cistern";
                     break;
                 case 2:
+                    outputLabel.Text = "This appears to be a storeroom pressed into service as living quarters. Two double bunks stand against the wall near the door, while barrels and crates fill the southern half of the chamber. There are 3 dirty scarlet cloaks hanging up on the bunks, and a set of lockpicks on top of a nearby crate.";
+                    roomLabel.Text = "Barracks";
+                    blueLabel.Text = "Take the lockpicks";
+                    redLabel.Text = "Search the barrels";
+                    yellowLabel.Text = "Put on a cloak";
+                    greenLabel.Text = "Return the the cellar";
                     break;
                 case 3:
+                    outputLabel.Text = "Thick dust covers the flagstones of this somber hallway. The walls are decorated with faux columns every ten feet, and the double doors at the west end of the hall are sheathed in copper plate, now green with age. A relief carving of a mournful angel graces the doors.";
+                    roomLabel.Text = "Crypt Hallway";
+                    blueLabel.Text = "Enter the double doors";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    greenLabel.Text = "Return to the cellar";
                     break;
                 case 4:
+                    outputLabel.Text = "Three large stone sarcophagi stand within this dusty crypt, and propped up against each sarcophagus is a human skeleton clad in bits of rusty mail. False columns along the walls are carved in the image of spreading oak trees. There is a door on the northern wall, and a door on the eastern wall.";
+                    roomLabel.Text = "Crypts";
+                    blueLabel.Text = "Investigate the skeletons";
+                    redLabel.Text = "Go east";
+                    yellowLabel.Text = "Go north";
+                    greenLabel.Text = "Return to the crypt hallway";
                     break;
                 case 5:
+                    outputLabel.Text = "This long room is partitioned into three areas, with iron bars walling off the north and south. Filthy straw lines the floors of those cells, the hinged doors of which are secured by chains and padlocks. A pair of disheveled human women are held in the cell to the south, while a human boy is confined to the north. All are dressed in plain gray tunics and have iron collars fitted around their necks. A heap of discarded clothing is piled carelessly against the far wall.";
+                    roomLabel.Text = "Prison";
+                    blueLabel.Text = "Pick the cell doors";
+                    redLabel.Text = "Try to force the cell doors";
+                    yellowLabel.Text = "";
+                    greenLabel.Text = "Return to the crypts";
                     break;
                 case 6:
+                    outputLabel.Text = "The door leads to a 15 foot long hallway. At the end of it is a locked door.";
+                    roomLabel.Text = "Armory Hallway";
+                    blueLabel.Text = "Pick the lock";
+                    redLabel.Text = "Try to force the door";
+                    yellowLabel.Text = "Enter the secret door";
+                    greenLabel.Text = "Return to the armory hallway";
+                    break;
+                case 7:
+                    outputLabel.Text = "Racks of weapons line the walls of this chamber, including spears, swords, crossbows, and bolts. A dozen dirty red cloaks hang from hooks by the door.";
+                    roomLabel.Text = "Armory";
+                    blueLabel.Text = "Put on the necklace/read map/something";
+                    redLabel.Text = "Put on a cloak";
+                    yellowLabel.Text = "";
+                    greenLabel.Text = "Return to the armory hallway";
+                    break;
+                case 8:
+                    outputLabel.Text = "This area is the north end of a large natural cavern, but it has been finished with dressed stone block walls and a flagstone floor. Several barrels are stored against the walls here, along with a number of empty crates, straw for packing, hammers, pry bars, and nails. The cavern continues for some distance to the south. You can make out several passages that open up off the larger cavern, and what looks like a deep pit or crevasse in the floor.";
+                    roomLabel.Text = "Storeroom";
+                    blueLabel.Text = "Search the barrels";
+                    redLabel.Text = "Enter the secret door";
+                    yellowLabel.Text = "Go south";
+                    greenLabel.Text = "Return to the armory hallway";
+                    break;
+                case 9:
+                    outputLabel.Text = "A cold breeze fills this large natural cavern, carrying with it the faint scent of decaying flesh. A crevasse divides the cavern and is flanked by two rough stone columns that support the twenty-foot-high ceiling. Two arched wooden bridges span the chasm. There are two hallways on the other side of the chasm. Both lead towards the east, one to the north and one to the south.";
+                    roomLabel.Text = "Crevasse";
+                    blueLabel.Text = "Go north";
+                    redLabel.Text = "Go east";
+                    yellowLabel.Text = "Go south";
+                    greenLabel.Text = "Return to the storeroom";
+                    break;
+                case 10:
+                    outputLabel.Text = "This barracks contains four roughly built wooden bunks, with heaped-up blankets and dirty dishes scattered about. A strong smell of unwashed bodies and rotten meat fills the air. Three tall, furry humanoids are lounging among the mess.";
+                    roomLabel.Text = "Guard Barracks";
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    greenLabel.Text = "Return to crevasse";
+                    break;
+                case 11:
+                    outputLabel.Text = "Several worn tables and chairs are scattered around this large room. Wooden benches are drawn up against walls decorated with draperies of brown and red, and several ale kegs are propped up and tapped. Four tough-looking human warriors wearing scarlet cloaks are gathered around one of the tables. A stack of coins and trinkets is heaped upon the tabletop between them.";
+                    roomLabel.Text = "Common Room";
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    greenLabel.Text = "Return to crevasse";
+                    break;
+                case 12:
+                    outputLabel.Text = "This room appears to be a wizard's workshop. A rat scurries across the floor and takes refuge under a large worktable set up with alembics, retorts, distillation coils, and other alchemical devices, all of it stewing and bubbling away. Bookshelves are crowded with sheaves of parchment and strange-looking tomes. There is a door along the eastern wall.";
+                    roomLabel.Text = "Wizard's Workshop";
+                    blueLabel.Text = "Go east";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    greenLabel.Text = "Return to crevasse";
+                    break;
+                case 13:
+                    outputLabel.Text = "The walls of this bedchamber are covered with drapes of scarlet cloth. The furnishings include a small writing desk with matching chair, a comfortable-looking bed, and a wooden chest at the foot of the bed. The chest is open, and mostly empty, as if someone in a rush had grabbed the contents and fled. There is a secret door slightly ajar on the opposite side of the room. Upon investigation, the passage leads back to the storeroom. It appears that glasstaff has fled.";
+                    roomLabel.Text = "Glasstaff's Quarters";
+                    blueLabel.Text = "";
+                    redLabel.Text = "";
+                    yellowLabel.Text = "";
+                    greenLabel.Text = "";
                     break;
                 default:
                     break;
